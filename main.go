3e7a7c59be9fd64290f55d5dec4ab9de76dde078
davidwah <davidwahyuyu@gmail.com>
2022-06-28 16:17:19 +0700

    add CORS pada main.go

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"construct-week1/config"
 	"construct-week1/factory"
 	"construct-week1/routes"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
@@ -12,5 +14,10 @@ func main() {
 	presenter := factory.InitFactory(dbConn)
 
 	e := routes.New(presenter)
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH, echo.OPTIONS},
+	}))
 	e.Logger.Fatal(e.Start(":80"))
 }
