ffe4504e0db53e8d6ba6006805b8a7872f16e535
davidwah <davidwahyuyu@gmail.com>
2022-06-29 21:24:36 +0700

    update cors ubah versi echo/v4

--- routes/routes.go
+++ routes/routes.go
@@ -5,10 +5,16 @@ import (
 	"construct-week1/middlewares"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func New(presenter factory.Presenter) *echo.Echo {
 	e := echo.New()
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT, echo.DELETE, echo.PATCH, echo.OPTIONS},
+	}))
 
 	//	Login
 	e.POST("/login", presenter.LoginPresenter.Login)
@@ -21,6 +27,6 @@ func New(presenter factory.Presenter) *echo.Echo {
 	e.POST("/products", presenter.ProductPresenter.AddProduct, middlewares.JWTMiddleware())
 	e.GET("/products", presenter.ProductPresenter.GetAllProduct, middlewares.JWTMiddleware())
 	e.GET("/products/:id", presenter.ProductPresenter.GetProductbyID, middlewares.JWTMiddleware())
-	
+
 	return e
 }
